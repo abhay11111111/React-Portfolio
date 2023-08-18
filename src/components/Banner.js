67b318126ee12906be93487e1f3f30e8f2c150cb
Abhay Kesarwani <56496714+abhay11111111@users.noreply.github.com>
2023-08-18 11:57:55 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -57,7 +57,7 @@ export const Banner = () => {
               <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
                 <span className="tagline">Welcome to my Portfolio</span>
                 <h1>{`Hi! I'm Abhay`} <span className="txt-rotate" dataPeriod="1000" data-rotate='[ "Web Developer", "Web Designer", "Freelancer"]'><span className="wrap">{text}</span></span></h1>
-                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                  <p>I'm a 2023 passout in Electronics and Communication from IET Lucknow current working as a Freelancer in HirePro.<\p>
                   <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
               </div>}
             </TrackVisibility>
